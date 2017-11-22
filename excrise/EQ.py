5b1a79ef222cfe795fbc737666e36332c07bb4e4
Sonic-boom <704482719@qq.com>
2017-11-22 17:33:18 +0800

    Add files via upload

--- excrise/EQ.py
+++ excrise/EQ.py
@@ -1,1 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  5 12:23:56 2017
 
+@author: akuma
+"""
+
+
+def conflict(state, nextX):
+    nextY = len(state)
+    for i in range(nextY):
+        if abs(state[i] - nextX) in (0, nextY - i):
+            return True
+    return False
+
+
+def queens(num=8, state=()):
+    for pos in range(num):
+        if not conflict(state, pos):
+            if len(state) == num - 1:
+                yield(pos,)
+            else:
+                for result in queens(num, state + (pos,)):
+                    yield(pos,) + result
+
+
+def prettyprint(solution):
+    def line(pos, length=len(solution)):
+        return '. ' * (pos) + 'X ' + '. ' * (length-pos-1)
+    for pos in solution:
+        print(line(pos))
