31a93cbe77df3cfdb52bc99332dceb6606b79351
sonic-boom <704482719@qq.com>
2018-04-16 17:30:05 +0800

    'update'

--- RT.py
+++ RT.py
@@ -3,3 +3,5 @@ b = int(input('b:'))
 c = int(input('c:'))
 if a*a+b*b == c*c or c*c+b*b == a*a or a*a+c*c == b*b:
     print(a, b, c, 'are right numbers')
+else:
+	print(a,b,c,'are not right numbers')
