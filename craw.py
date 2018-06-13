e0968914a5c173680c09e16cbe80abab86ab63f0
sonic-boom <704482719@qq.com>
2018-06-13 21:21:57 +0800

    pep8

--- craw.py
+++ craw.py
@@ -3,7 +3,7 @@ import urllib.parse
 
 url = 'http://www.douyu.com'
 header = {
-   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'
 }
 
 request = urllib.request.Request(url, headers=header)
